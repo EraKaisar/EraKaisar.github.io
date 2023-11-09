cac346922a5390289d92ad5efb75ca9e51208f0e
Deadline <erakaisar25@gmail.com>
2023-11-09 22:34:05 +0600

    new file:   blog.html new file:   bootstrap/bootstrap.bundle.min.js new file:   bootstrap/bootstrap.min.css new file:   images/1+1.jpg new file:   images/1.jpg new file:   images/2.jpg new file:   images/3.jpg new file:   images/Iphone2.jpg new file:   images/JavaScript Intro.pdf new file:   images/Week 7 - JavaScript Intermediate.pdf new file:   images/about.jpg new file:   images/amazon.jpg new file:   images/back.jpg new file:   images/banner1-1.jpg new file:   images/banner2.jpg new file:   images/banner2.webp new file:   images/banner3-3.webp new file:   images/banner3.jpg new file:   images/bg.jpg new file:   images/blogbg.jpg new file:   images/camera.jpg new file:   images/cofee.jpg new file:   images/dust.jpg new file:   images/favicon.ico new file:   images/features/f1.png new file:   images/features/f2.png new file:   images/features/f3.png new file:   images/features/f4.png new file:   images/features/f5.png new file:   images/features/f6.png new file:   images/fridge.jpg new file:   images/google.png new file:   images/iphone.jpg new file:   images/iphone3.jpg new file:   images/iphone3.webp new file:   images/iphone4.jpg new file:   images/iphone5.jpg new file:   images/konder.jpg new file:   images/laptop.jpg new file:   images/laptop2.jpg new file:   images/logo.png new file:   images/logo1.png new file:   images/me.jpg new file:   images/oppo.jpg new file:   images/pan.jpg new file:   images/ps.jpg new file:   images/sm2.jpg new file:   images/sm2.webp new file:   images/tv.jpg new file:   images/watches.jpg new file:   images/wmachine.jpg new file:   images/yandex.jpg new file:   index.html new file:   kino.mp3 new file:   script.js new file:   shop.html new file:   sproduct.html new file:   style.css

---
+++ script.js
@@ -0,0 +1,67 @@
+/*const bar = document.getElementById("bar");
+const close = document.getElementById('close');
+const nav = document.getElementById("navbar");
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add("active");
+    })
+}
+
+if(close){
+    bar.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}*/
+
+const bar = document.getElementById("bar")
+const nav = document.getElementById("navbar")
+const close = document.getElementById("close")
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add("active")
+    });
+}
+
+if(close){
+    bar.addEventListener('click', () => {
+        nav.classList.remove("active")
+    });
+}
+
+
+
+
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+}
+
+
+//Music
+
+var audio = document.getElementById('audio');
+var playButton = document.getElementById('playButton');
+var stopButton = document.getElementById('stopButton');
+
+playButton.addEventListener('click', function() {
+    audio.play();
+});
+
+stopButton.addEventListener('click', function() {
+    audio.pause();
+    audio.currentTime = 0;
+});
+
+
